a501afc0552257f2a57e527db7d5d07c0d6589fd
Luara Kerlen dos Santos Rangel <luarakerlen@hotmail.com>
2021-07-20 00:38:58 -0300

    Highlight Cards

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,10 +1,58 @@
 import React from "react";
-import { Container, Title } from './styles'
+import { HighlightCard } from "../../components/HighlightCard";
+import {
+	Container,
+	Header,
+	UserWrapper,
+	UserInfo,
+	Photo,
+	User,
+	UserGreeting,
+	UserName,
+	Icon,
+	HighlightCards,
+} from "./styles";
 
 export function Dashboard() {
 	return (
 		<Container>
-			<Title>Dashboard</Title>
+			<Header>
+				<UserWrapper>
+					<UserInfo>
+						<Photo
+							source={{
+								uri: "https://avatars.githubusercontent.com/u/26902816?v=4",
+							}}
+						/>
+						<User>
+							<UserGreeting>Olá,</UserGreeting>
+							<UserName>Luara</UserName>
+						</User>
+					</UserInfo>
+					<Icon name="power" />
+				</UserWrapper>
+			</Header>
+
+			<HighlightCards>
+				<HighlightCard
+					type="up"
+					title="Entradas"
+					amount="R$ 17.400,00"
+					lastTransaction="Última entrada dia 13 de abril"
+				/>
+				<HighlightCard
+					type="down"
+					title="Saídas"
+					amount="R$ 1.500,00"
+					lastTransaction="Última saída dia 3 de abril"
+				/>
+				<HighlightCard
+					type="total"
+					title="Total"
+					amount="R$ 15.400,00"
+					lastTransaction="01 à 16 de abril"
+				/>
+			</HighlightCards>
 		</Container>
 	);
 }
