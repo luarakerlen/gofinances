1fcab223c2a17aa9a4cacf77cd54d0cde9b38ee6
Luara Kerlen dos Santos Rangel <luarakerlen@hotmail.com>
2021-07-29 16:43:03 -0300

    History Card

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
-import { Platform } from 'react-native';
-import { Dashboard } from '../screens/Dashboard';
-import { Register } from '../screens/Register';
-import { useTheme } from 'styled-components';
 import { MaterialIcons } from '@expo/vector-icons';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import React from 'react';
+import { Platform } from 'react-native';
+import { useTheme } from 'styled-components';
+import { Dashboard } from '../screens/Dashboard';
+import { Register } from '../screens/Register';
+import { Resume } from '../screens/Resume';
 
 const { Navigator, Screen } = createBottomTabNavigator();
 
@@ -46,7 +47,7 @@ export function AppRoutes() {
 			/>
 			<Screen
 				name='Resumo'
-				component={Register}
+				component={Resume}
 				options={{
 					tabBarIcon: ({ size, color }) => (
 						<MaterialIcons name='pie-chart' size={size} color={color} />
