b1073cfa0963f132eb44655729f68726ea341e0c
Luara Kerlen dos Santos Rangel <luarakerlen@hotmail.com>
2021-07-29 17:02:38 -0300

    Fix text on emission date CNH

--- src/screens/Resume/index.tsx
+++ src/screens/Resume/index.tsx
@@ -1,19 +1,56 @@
 import React, { useState, useEffect } from 'react';
 import { HistoryCard } from '../../components/HistoryCard';
 import { Container, Header, Title } from './styles';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { categories } from '../../utils/categories';
+
+interface TransactionDataProps {
+	type: 'positive' | 'negative';
+	name: string;
+	amount: string;
+	category: string;
+	date: string;
+}
 
 export function Resume() {
+	async function loadData() {
+		const dataKey = '@gofinance:transactions';
+		const response = await AsyncStorage.getItem(dataKey);
+		const responseFormatted = response ? JSON.parse(response) : [];
+
+		const expensives = responseFormatted.filter(
+			(expensive: TransactionDataProps) => expensive.type === 'negative'
+		);
+
+		const totalByCategory = [];
+
+		categories.forEach(category => {
+			let categorySum = 0;
+
+			expensives.forEach((expensive: TransactionDataProps) => {
+				if(expensive.category === category.key) {
+					categorySum += Number(expensive.amount);
+				}
+			});
+
+			totalByCategory.push({
+				name: category.name,
+				total: categorySum,
+			})
+		});
+	}
+
+	useEffect(() => {
+		loadData();
+	});
+
 	return (
 		<Container>
 			<Header>
 				<Title>Resumo por categoria</Title>
 			</Header>
 
-			<HistoryCard
-				title="Compras"
-				amount="R$ 150,50"
-				color="red"
-			/>
+			<HistoryCard title='Compras' amount='R$ 150,50' color='red' />
 		</Container>
 	);
 }
