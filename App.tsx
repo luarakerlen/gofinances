21ae2a2377ab830b5cabee6214cb8c521d7ee82d
Luara Kerlen dos Santos Rangel <luarakerlen@hotmail.com>
2021-12-20 23:42:15 -0300

    create context

--- App.tsx
+++ App.tsx
@@ -1,22 +1,23 @@
-import React from "react";
+import React from 'react';
 import 'react-native-gesture-handler';
 import 'intl';
 import 'intl/locale-data/jsonp/pt-BR';
-import { ThemeProvider } from "styled-components";
-import { NavigationContainer } from "@react-navigation/native";
-import AppLoading from "expo-app-loading";
+import { ThemeProvider } from 'styled-components';
+import { NavigationContainer } from '@react-navigation/native';
+import AppLoading from 'expo-app-loading';
 import {
 	useFonts,
 	Poppins_400Regular,
 	Poppins_500Medium,
-	Poppins_700Bold
-} from '@expo-google-fonts/poppins'
+	Poppins_700Bold,
+} from '@expo-google-fonts/poppins';
 
-import theme from "./src/global/styles/theme";
+import theme from './src/global/styles/theme';
 
-import { AppRoutes } from "./src/routes/app.routes";
-import { StatusBar } from "react-native";
-import { SignIn } from "./src/screens/SignIn";
+import { AppRoutes } from './src/routes/app.routes';
+import { StatusBar } from 'react-native';
+import { SignIn } from './src/screens/SignIn';
+import { AuthProvider } from './src/hooks/auth';
 
 export default function App() {
 	const [fontsLoaded] = useFonts({
@@ -25,15 +26,17 @@ export default function App() {
 		Poppins_700Bold,
 	});
 
-	if(!fontsLoaded) {
-		return <AppLoading/>
+	if (!fontsLoaded) {
+		return <AppLoading />;
 	}
 
 	return (
 		<ThemeProvider theme={theme}>
 			<NavigationContainer>
-				<StatusBar barStyle='light-content'/>
-				<SignIn />
+				<StatusBar barStyle='light-content' />
+				<AuthProvider>
+					<SignIn />
+				</AuthProvider>
 			</NavigationContainer>
 		</ThemeProvider>
 	);
