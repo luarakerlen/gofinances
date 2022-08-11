c848d465b346f9b0d5685708ad779b393c2b2d81
Luara Kerlen dos Santos Rangel <luarakerlen@hotmail.com>
2022-08-11 20:28:42 -0300

    remove google sign in

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -46,36 +46,55 @@ function AuthProvider({ children }: AuthProviderProps) {
 	const userStorageKey = '@gofinances:user';
 
 	async function signIWithGoogle() {
-		try {
-			const RESPONSE_TYPE = 'token';
-			const SCOPE = encodeURI('profile email');
+		const userLogged = {
+			id: 'luara',
+			email: 'luarakerlen@hotmail.com',
+			name: 'Luara',
+			photo: '',
+		};
 
-			const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${SCOPE}`;
-
-			const { type, params } = (await AuthSession.startAsync({
-				authUrl,
-			})) as AuthorizationResponse;
-
-			if (type === 'success') {
-				const response = await fetch(
-					`https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=${params.access_token}`
-				);
-				const userInfo = await response.json();
-
-				const userLogged = {
-					id: String(userInfo.id),
-					email: userInfo.email!,
-					name: userInfo.name!,
-					photo: userInfo.picture!,
-				};
-
-				setUser(userLogged);
-				await AsyncStorage.setItem(userStorageKey, JSON.stringify(userLogged));
-			}
-		} catch (error) {
-			throw new Error(error);
-		}
+		setUser(userLogged);
+		await AsyncStorage.setItem(userStorageKey, JSON.stringify(userLogged));
 	}
+
+	// async function signIWithGoogle() {
+	// 	try {
+	// 		const RESPONSE_TYPE = 'token';
+	// 		const SCOPE = encodeURI('profile email');
+
+	// 		const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${SCOPE}`;
+
+	// 		const { type, params } = (await AuthSession.startAsync({
+	// 			authUrl,
+	// 		})) as AuthorizationResponse;
+
+	// 		if (type === 'success') {
+	// 			const response = await fetch(
+	// 				`https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=${params.access_token}`
+	// 			);
+	// 			const userInfo = await response.json();
+
+	// 			// const userLogged = {
+	// 			// 	id: String(userInfo.id),
+	// 			// 	email: userInfo.email!,
+	// 			// 	name: userInfo.name!,
+	// 			// 	photo: userInfo.picture!,
+	// 			// };
+
+	// 			const userLogged = {
+	// 				id: 'luara',
+	// 				email: 'luarakerlen@hotmail.com',
+	// 				name: 'Luara',
+	// 				photo: '',
+	// 			};
+
+	// 			setUser(userLogged);
+	// 			await AsyncStorage.setItem(userStorageKey, JSON.stringify(userLogged));
+	// 		}
+	// 	} catch (error) {
+	// 		throw new Error(error);
+	// 	}
+	// }
 
 	async function signIWithApple() {
 		try {
