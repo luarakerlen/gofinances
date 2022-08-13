1fe303b3877f13ac7838a20d6dcd916c4203f01a
Luara Kerlen dos Santos Rangel <luarakerlen@hotmail.com>
2022-08-12 22:47:54 -0300

    tests with mocks

--- src/hooks/auth.spec.tsx
+++ src/hooks/auth.spec.tsx
@@ -1,21 +1,24 @@
 import { renderHook, act } from '@testing-library/react-hooks';
 import { AuthProvider, useAuth } from './auth';
+import { mocked } from 'jest-mock';
+import { startAsync } from 'expo-auth-session';
 
-jest.mock('expo-auth-session', () => {
-	return {
-		startAsync: () => {
-			return {
-				type: 'success',
-				params: {
-					access_token: 'google-token',
-				},
-			};
-		},
-	};
-});
+jest.mock('expo-auth-session');
 
 describe('Auth Hook', () => {
+	beforeEach(() => {
+		jest.clearAllMocks();
+	});
+
 	it('should be able to sign in with google account existing', async () => {
+		const googleMocked = mocked(startAsync as any);
+		googleMocked.mockReturnValue({
+			type: 'success',
+			params: {
+				access_token: 'google-token',
+			},
+		});
+
 		global.fetch = jest.fn(() =>
 			Promise.resolve({
 				json: () =>
@@ -37,7 +40,22 @@ describe('Auth Hook', () => {
 		await act(() => result.current.signIWithGoogle());
 
 		const user = result.current.user;
-    console.log('user: ', user)
 		expect(user).toBeTruthy();
+	});
+
+	it('user should not connect if cancel authentication with google', async () => {
+		const googleMocked = mocked(startAsync as any);
+		googleMocked.mockReturnValue({
+			type: 'cancel',
+		});
+
+		const { result } = renderHook(() => useAuth(), {
+			wrapper: AuthProvider,
+		});
+
+		await act(() => result.current.signIWithGoogle());
+
+		const user = result.current.user;
+		expect(user).not.toHaveProperty('id');
 	});
 });
